f807e4de7d62320bec6ed5cd0ca8a987fa9bc843
Colleen Murphy <colleen@gazlene.net>
2017-06-11 22:13:35 +0200

    Remove unnecessary beaker tests
    It is good enough to assume that if puppet ran successfully then it also
    was able to sucessfully find and install packages and apply config
    files. For the sake of maintainability, this patch removes beaker tests
    for individual files and packages. It also removes a check for a cron
    that was already deleted from the manifest.
    
    Change-Id: Ia8b29684213bc8c96d23fb9dd53baf8e65c85362

--- spec/acceptance/basic_spec.rb
+++ spec/acceptance/basic_spec.rb
@@ -29,61 +29,6 @@ describe 'puppet-lodgeit module', :if => ['debian', 'ubuntu'].include?(os[:famil
     apply_manifest(default_puppet_module, catch_changes: true)
   end
 
-  describe 'required files' do
-    describe file('/srv/lodgeit') do
-      it { should be_directory }
-    end
-
-    describe file('/tmp/lodgeit-main/.git') do
-      it { should be_directory }
-    end
-
-    describe file('/srv/lodgeit/acceptance/.git') do
-      it { should be_directory }
-    end
-
-    describe file('/etc/init/acceptance-paste.conf') do
-      it { should be_file }
-      its(:content) { should include 'exec python /srv/lodgeit/acceptance/manage.py runserver -h 127.0.0.1 -p 80' }
-    end
-
-    describe file('/srv/lodgeit/acceptance/lodgeit/static/header-bg2.png') do
-      it { should be_file }
-    end
-
-    describe file('/srv/lodgeit/acceptance/manage.py') do
-      it { should be_file }
-      its(:content) { should include "dburi = 'mysql://acceptance:123456@localhost:3306/acceptance'" }
-    end
-
-    describe file('/srv/lodgeit/acceptance/lodgeit/views/layout.html') do
-      it { should be_file }
-      its(:content) { should include 'Acceptance Pastebin' }
-    end
-  end
-
-  describe 'required packages' do
-    required_packages = [
-      package('python-imaging'),
-      package('python-jinja2'),
-      package('python-pybabel'),
-      package('python-werkzeug'),
-      package('python-simplejson'),
-      package('python-pygments'),
-      package('python-mysqldb'),
-    ]
-
-    required_packages.each do |package|
-      describe package do
-        it { should be_installed }
-      end
-    end
-
-    describe package('SQLAlchemy') do
-      it { should be_installed.by('pip') }
-    end
-  end
-
   describe 'required services' do
     describe service('acceptance-paste') do
       it { should be_running }
@@ -95,7 +40,4 @@ describe 'puppet-lodgeit module', :if => ['debian', 'ubuntu'].include?(os[:famil
     end
   end
 
-  describe cron do
-    it { should have_entry('0 0 * * * /usr/bin/mysqldump --defaults-file=/root/.acceptance_db.cnf --opt --ignore-table mysql.event --all-databases | gzip -9 > /var/backups/mysql_backups/acceptance.sql.gz').with_user('root') }
-  end
 end
